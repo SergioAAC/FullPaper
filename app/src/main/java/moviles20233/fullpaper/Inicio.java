c3ecccdfafd21175d2ccbff06883ce77a6a42638
Bygyacqq <144375079+Bygyacqq@users.noreply.github.com>
2023-10-25 13:42:49 -0500

    interfaz

---
+++ app/src/main/java/moviles20233/fullpaper/Inicio.java
@@ -0,0 +1,14 @@
+package moviles20233.fullpaper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Inicio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio);
+    }
+}
--- app/src/main/java/moviles20233/fullpaper/MainActivity.java
+++ app/src/main/java/moviles20233/fullpaper/MainActivity.java
@@ -2,13 +2,41 @@ package moviles20233.fullpaper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Button btnRegistrarse = findViewById(R.id.button4);
+
+
+        btnRegistrarse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, Registrarse.class);
+                startActivity(intent);
+            }
+        });
+
+
+        Button btnIniciarSesion = findViewById(R.id.Ventana1);
+
+        btnIniciarSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, Login.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
